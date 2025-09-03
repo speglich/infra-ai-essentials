48780e63b903fbb767dfe0daee60364317a0ac97
João Speglich <speglichj@gmail.com>
2025-09-03 16:36:48 -0300

    provisioning: add auto accept to setup-docker

--- provisioning/docker/setup-docker-nvidia-ol8.sh
+++ provisioning/docker/setup-docker-nvidia-ol8.sh
@@ -12,11 +12,11 @@ fi
 # Install required packages
 echo "setup repositories..."
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf config-manager -y --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
 # Install Docker
 echo "Installing Docker..."
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 if [ $? -ne 0 ]; then
     echo "Failed to install Docker. Exiting."
